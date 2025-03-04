9db722f2b635ae8ad704203b47f537c2b0b9a11a
unknown <sanchitrana0410@gmail.com>
2025-03-05 00:46:36 +0530

    Working Initial Prototype

---
+++ app/src/main/java/com/example/medsewa/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.medsewa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.medsewa.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.post {
+            val navController = findNavController(R.id.fragmentContainerView)
+            binding.bottomNavigationView.setupWithNavController(navController)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/medsewa/fragments/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.medsewa.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.medsewa.R
+import com.example.medsewa.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+
+lateinit var  binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
